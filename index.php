f1b4e6055b729246b4f2c882ae86cf60b75023d0
Unknown <jonaskkristensen@hotmail.dk>
2017-03-27 13:14:00 +0200

    Buttons added

--- index.php
+++ index.php
@@ -9,7 +9,35 @@
 
 <script>var __adobewebfontsappname__="dreamweaver"</script>
 <script src="http://use.edgefonts.net/source-sans-pro:n2:default.js" type="text/javascript"></script>
+<style>
+.btn {
+  background: #3463d9;
+  background-image: -webkit-linear-gradient(top, #3463d9, #a836a8);
+  background-image: -moz-linear-gradient(top, #3463d9, #a836a8);
+  background-image: -ms-linear-gradient(top, #3463d9, #a836a8);
+  background-image: -o-linear-gradient(top, #3463d9, #a836a8);
+  background-image: linear-gradient(to bottom, #3463d9, #a836a8);
+  -webkit-border-radius: 28;
+  -moz-border-radius: 28;
+  border-radius: 28px;
+  text-shadow: 7px 6px 5px #000000;
+  font-family: Arial;
+  color: #ffffff;
+  font-size: 60px;
+  padding: 7px 20px 10px 20px;
+  text-decoration: none;
+}
 
+.btn:hover {
+  background: #d2f50a;
+  background-image: -webkit-linear-gradient(top, #d2f50a, #de1b1b);
+  background-image: -moz-linear-gradient(top, #d2f50a, #de1b1b);
+  background-image: -ms-linear-gradient(top, #d2f50a, #de1b1b);
+  background-image: -o-linear-gradient(top, #d2f50a, #de1b1b);
+  background-image: linear-gradient(to bottom, #d2f50a, #de1b1b);
+  text-decoration: none;
+}
+</style>
 </head>
 
 
@@ -23,7 +51,14 @@
   
   
  
+ <section style="width:100%; height:500px; background-color:white; text-align: center; margin-top: 20px;">
+ <h1 style="font-size: 50px;">Er du over 18 år?</h1>
  
+ <button class="btn" onclick="<?php echo "Hello world"; ?>">JA</button>
+ 
+ <button class="btn" onclick="<?php echo "Hello world"; ?>">NEJ</button>
+ 
+ </section>
  
  
   <!-- Footer Section -->
