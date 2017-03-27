120f2fbcd1f3267e6639825afc769ed79cf529a7
Unknown <jonaskkristensen@hotmail.dk>
2017-03-27 13:32:43 +0200

    Btn fixed

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html lang="en-US">
 <head>
 <meta charset="UTF-8">
+<meta name="robots" content="noindex, nofollow">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title>Jonas & Christoffer</title>
@@ -48,8 +49,7 @@
   <!-- Navigation -->
   
 <?php include 'header.php';?>
-  
-  
+ 
  
  <section style="width:100%; height:500px; background-color:white; text-align: center; margin-top: 20px;">
  <h1 style="font-size: 50px;">Er du over 18 år?</h1>
@@ -57,12 +57,13 @@
  <button class="btn" onclick="<?php echo "Hello world"; ?>">JA</button>
  
  <button class="btn" onclick="<?php echo "Hello world"; ?>">NEJ</button>
- 
  </section>
+
+ 
  
  
   <!-- Footer Section -->
-  <?php include 'footer.php';?>
+  <?php include 'footer.php'; ?>
  
  
 <!-- Main Container Ends -->
